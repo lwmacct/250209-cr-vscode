8f1e7343729da8fa6d3a86a1bb9eccb42f5a9068
lwmacct <lwmacct@icloud.com>
2025-04-27 13:20:54 +0800

    -

--- dev-2504260/apps/file/root/.zshrc.d/env.sh
+++ dev-2504260/apps/file/root/.zshrc.d/env.sh
@@ -1,8 +1,36 @@
 # shellcheck disable=all
 
-__env() {
-  _env_file="/root/.env"
-  source $_env_file
-  export $(cat $_env_file | awk -F= '{print $1}' | xargs -r -I {} echo {} | xargs)
+__load_taskfile_env() {
+  {
+    _task_env="$(find /apps/data/workspace -type f -path '*/.taskfile/.env.template' 2>/dev/null)"
+    while IFS= read -r _env_file; do
+      set -a
+      source $_env_file
+      set +a
+    done <<<"$_task_env"
+  }
+
+  {
+    _task_env="$(find /apps/data/workspace -type f -path '*/.taskfile/.env' 2>/dev/null)"
+    while IFS= read -r _env_file; do
+      set -a
+      source $_env_file
+      set +a
+    done <<<"$_task_env"
+  }
+
 }
-__env
+
+__main() {
+  # 如有必要, 可以取消注释
+  # __load_taskfile_env
+
+  {
+    _env_file="/root/.env"
+    set -a
+    source $_env_file
+    set +a
+  }
+
+}
+__main
