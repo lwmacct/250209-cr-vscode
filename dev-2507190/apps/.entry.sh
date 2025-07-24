3e56c324f08f33132a8cf80adb79d8f14a93d7ec
lwmacct <lwmacct@icloud.com>
2025-07-25 01:41:05 +0800

    -

--- dev-2507190/apps/.entry.sh
+++ dev-2507190/apps/.entry.sh
@@ -2,23 +2,23 @@
 # Admin https://www.yuque.com/lwmacct
 
 __main() {
-    {
-        : # 初始化文件
-        mkdir -p /apps/data/{workspace,logs,script,cron.d}
-        tar -vcpf - -C /apps/file . | (cd / && tar -xpf - --skip-old-files)
-        (cd /apps/data/workspace && go work init)
-    } 2>&1 | tee /apps/data/logs/entry-tar.log
+  {
+    : # 初始化文件
+    mkdir -p /apps/data/{workspace,logs,script,cron.d}
+    tar -vcpf - -C /apps/file . | (cd / && tar -xpf - --skip-old-files)
+    (cd /apps/data/workspace && go work init)
+  } 2>&1 | tee /apps/data/logs/entry-tar.log
 
-    {
-        for _script in /apps/data/init.d/*.sh; do
-            if [ -r "$_script" ]; then
-                echo "Run $_script"
-                # shellcheck disable=SC1090
-                source "$_script"
-            fi
-        done
-    } 2>&1 | tee -a /apps/data/logs/entry-init.log
-    service cron reload
+  {
+    for _script in /apps/data/init.d/*.sh; do
+      if [ -r "$_script" ]; then
+        echo "Run $_script"
+        # shellcheck disable=SC1090
+        source "$_script"
+      fi
+    done
+  } 2>&1 | tee -a /apps/data/logs/entry-init.log
+  service cron reload
 }
 
 __main
--- dev-2507250/apps/file/apps/data/init.d/00-cron.sh
+++ dev-2507250/apps/file/apps/data/init.d/00-cron.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env bash
+
+cat >/apps/data/supervisor.d/cron.conf <<EOF
 [program:cron]
 command=cron -f
 autostart=true
@@ -12,3 +15,4 @@ stderr_logfile=/var/log/supervisor/cron_error.log
 stderr_logfile_maxbytes=50MB
 stderr_logfile_backups=10
 environment=TERM="xterm"
+EOF
