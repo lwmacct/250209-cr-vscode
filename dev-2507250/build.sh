e4f2ff30538e0f6c64c590d0af5510700d763035
lwmacct <lwmacct@icloud.com>
2025-07-25 02:00:30 +0800

    使用手动下载方式安装 uv 到 /usr/local/bin

--- dev-2507250/build.sh
+++ dev-2507250/build.sh
@@ -104,8 +104,12 @@ RUN set -eux; \
 
 RUN set -eux; \
     echo "安装 uv"; \
-    XDG_BIN_HOME=/usr/local/bin curl -LsSf https://astral.sh/uv/install.sh | sh; \
-    export PATH="/root/.local/bin:$PATH"; \
+    wget -qO- https://github.com/astral-sh/uv/releases/download/0.8.2/uv-x86_64-unknown-linux-gnu.tar.gz | tar -xzf - -C /tmp; \
+    mv /tmp/uv-x86_64-unknown-linux-gnu/uv /usr/local/bin/uv; \
+    mv /tmp/uv-x86_64-unknown-linux-gnu/uvx /usr/local/bin/uvx; \
+    chmod +x /usr/local/bin/uv /usr/local/bin/uvx; \
+    rm -rf /tmp/uv-x86_64-unknown-linux-gnu; \
+    export PATH="/usr/local/bin:$PATH"; \
     uv venv /opt/venv --system-site-packages; \
     uv pip install --python /opt/venv/bin/python pip; \
     /opt/venv/bin/pip config set global.index-url https://mirrors.ustc.edu.cn/pypi/simple; \
