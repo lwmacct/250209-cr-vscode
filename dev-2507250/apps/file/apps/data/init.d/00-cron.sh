bb3fd0f627e9a2360b23c6037a8a98f1a26afa5a
lwmacct <lwmacct@icloud.com>
2025-07-25 15:07:44 +0800

    -

--- dev-2507250/apps/file/apps/data/init.d/00-cron.sh
+++ dev-2507250/apps/file/apps/data/init.d/00-cron.sh
@@ -8,11 +8,11 @@ autorestart=true
 startretries=3
 user=root
 redirect_stderr=true
-stdout_logfile=/var/log/supervisor/cron.log
-stdout_logfile_maxbytes=50MB
-stdout_logfile_backups=10
-stderr_logfile=/var/log/supervisor/cron_error.log
-stderr_logfile_maxbytes=50MB
-stderr_logfile_backups=10
+stdout_logfile=/apps/data/logs/cron.stdout.log
+stdout_logfile_maxbytes=10MB
+stdout_logfile_backups=3
+stderr_logfile=/apps/data/logs/cron.stderr.log
+stderr_logfile_maxbytes=10MB
+stderr_logfile_backups=3
 environment=TERM="xterm"
 EOF
